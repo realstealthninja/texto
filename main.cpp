01caec3d9c2c5b303e33601cc6e7c5ba22abbf77
realstealthninja <realstealthninja@gmail.com>
2023-04-08 13:27:50 +0530

    fix: make main.cpp take both images and videos

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <regex>
 #include "render.hpp"
 
 using namespace std;
 
-int main(int argc, char ** argv) {
+regex RE_IMAGE("(.*\\.(jpe?g|png|bmp)$)");
+
+int main(int argc, char** argv) {
     if (argc != 3) {
         cout << "Not enough arguments!";
         return -1;
     }
 
-    render(argv[1]);
+    if (regex_match(argv[1], RE_IMAGE))
+        renderImage(argv[1]);
+    else
+        renderVideo(argv[1]);
 
     return 0;
 }

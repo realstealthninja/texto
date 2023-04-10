c0abbfae163b2ec51030675d99e3eb8767a70b69
realstealthninja <realstealthninja@gmail.com>
2023-04-10 23:20:17 +0530

    fix: adding checks for implementing colour

--- main.cpp
+++ main.cpp
@@ -7,15 +7,17 @@ using namespace std;
 regex RE_IMAGE("(.*\\.(jpe?g|png|bmp)$)");
 
 int main(int argc, char** argv) {
+    EncodeType encodeType = GSCALE;
     if (argc != 3) {
         cout << "Not enough arguments!";
         return -1;
     }
-
+    if (strcmp(argv[2], "GSCALE") != 0)
+        encodeType = RGB;
     if (regex_match(argv[1], RE_IMAGE))
-        renderImage(argv[1]);
+        renderImage(argv[1], encodeType);
     else
-        renderVideo(argv[1]);
+        renderVideo(argv[1], encodeType);
 
     return 0;
 }

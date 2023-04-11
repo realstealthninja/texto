960312baf6d506b2f54941787cb52b3e6b91c7dc
realstealthninja <realstealthninja@gmail.com>
2023-04-11 11:57:14 +0530

    fix: fixing no file named scales error

---
+++ scales.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef TEXTO_SCALES_HPP
+#define TEXTO_SCALES_HPP
+
+std::string SHORT_GRAYSCALE = " .:-=+*#%@";
+std::string GRAYSCALE = " .'`^\",:;Il!i><~+_-?][}{1)(|/\\tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+std::string REVERSED_GRAYSCALE = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+#endif //TEXTO_SCALES_HPP

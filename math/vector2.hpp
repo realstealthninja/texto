d14143f993b105c603ea07b785a923690ded8b79
realstealthninja <realstealthninja@gmail.com>
2023-04-11 15:30:34 +0530

    fix: creating vector2 class

---
+++ math/vector2.hpp
@@ -0,0 +1,23 @@
+#ifndef TEXTO_VECTOR2_HPP
+#define TEXTO_VECTOR2_HPP
+
+
+class Vector2 {
+    public:
+        float x,y;
+
+        explicit Vector2(float x=0, float y=0);
+        Vector2(const Vector2& vector);
+
+        Vector2 operator+ (const Vector2 &v) const;
+        Vector2 & operator+= (const Vector2 &v);
+        Vector2 operator- (const Vector2 &v) const;
+        Vector2& operator-= (const Vector2 &v);
+
+        float &operator[](int i);
+
+
+};
+
+#include "vector2.cpp"
+#endif //TEXTO_VECTOR2_HPP

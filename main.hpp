84ed2d61dd211c78046dd6e97636ad1c6f11d829
realstealthninja <realstealthninja@gmail.com>
2023-04-13 21:06:29 +0530

    fix: adding main.hpp

---
+++ main.hpp
@@ -0,0 +1,25 @@
+//
+// Created by stealthninja on 4/13/23.
+//
+
+#ifndef TEXTO_MAIN_HPP
+#define TEXTO_MAIN_HPP
+
+std::string MENU = "############################\n"
+                   "#                          #\n"
+                   "#      0.Image             #\n"
+                   "#      1.Video             #\n"
+                   "#                          #\n"
+                   "############################\n"
+                   "Input?: ";
+std::string ENCODING = "############################\n"
+                       "#                          #\n"
+                       "#      0.GRAY SCALE        #\n"
+                       "#      1.RGB               #\n"
+                       "#      2.SHORT GRAY        #\n"
+                       "#      3.REVERSE GRAY      #\n"
+                       "#                          #\n"
+                       "############################\n"
+                       "Input?: ";
+
+#endif //TEXTO_MAIN_HPP

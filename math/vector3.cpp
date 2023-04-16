e768e3d354a56507e9a2807c5e0255d03620837e
realstealthninja <realstealthninja@gmail.com>
2023-04-16 17:40:49 +0530

    fix: implement transform coordinate
    code translated from sharpDX

--- math/vector3.cpp
+++ math/vector3.cpp
@@ -1,4 +1,5 @@
 #include "vector3.hpp"
+#include "Vector4.hpp"
 #include <cassert>
 
 Vector3::Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
@@ -36,4 +37,13 @@ float &Vector3::operator[](int i) {
             return z;
 
     }
+}
+Vector3 Vector3::TransformCoordinate(Vector3 &coordinate, Matrix<float> &transform) {
+    Vector4 vector = Vector4();
+    vector.x = (coordinate.x * transform(1,1)) + (coordinate.y * transform(2,1)) + (coordinate.z * transform(3,1)) + transform(4,1);
+    vector.y = (coordinate.x * transform(1,2)) + (coordinate.y * transform(2,2)) + (coordinate.z * transform(3,2)) + transform(4,2);
+    vector.z = (coordinate.x * transform(1,3)) + (coordinate.y * transform(2,3)) + (coordinate.z * transform(3,3)) + transform(4,3);
+    vector.w = 1.0f / (coordinate.x * transform(1,4)) + (coordinate.y * transform(2,4)) + (coordinate.z * transform(3,4)) + transform(4,4);
+
+    return Vector3(vector.x * vector.w, vector.y * vector.w, vector.z * vector.w);
 }

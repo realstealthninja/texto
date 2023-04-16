91999a3277b9950062e04b083376dc2239fa548d
realstealthninja <realstealthninja@gmail.com>
2023-04-16 16:32:12 +0530

    fix: add boiler plate for vector3

--- math/vector3.hpp
+++ math/vector3.hpp
@@ -2,9 +2,19 @@
 #define TEXTO_VECTOR3_HPP
 
 
-class vector3 {
+class Vector3 {
+public:
+    float x, y, z;
+    explicit Vector3(float x = 0, float y = 0, float z = 0);
+    Vector3(const Vector3 &vector);
 
+    Vector3 operator+(const Vector3 &v) const;
+    Vector3 &operator+=(const Vector3 &v);
+    Vector3 operator-(const Vector3 &v) const;
+    Vector3 &operator-=(const Vector3 &v);
+
+    float &operator[](int i);
 };
 
 
-#endif //TEXTO_VECTOR3_HPP
+#endif//TEXTO_VECTOR3_HPP

d716e20d72dff569f98dc98fd1c81ebc526e02d4
realstealthninja <realstealthninja@gmail.com>
2023-04-17 21:29:24 +0530

    fix: lower the vector

--- math/vector2.cpp
+++ math/vector2.cpp
@@ -1,30 +1,30 @@
 #include "vector2.hpp"
 #include "cassert"
 
-Vector2::Vector2(float x, float y) : x(x), y(y){};
-Vector2::Vector2(const Vector2 &vector) : x(vector.x), y(vector.y) {}
+vector2::vector2(float x, float y) : x(x), y(y){};
+vector2::vector2(const vector2 &vector) : x(vector.x), y(vector.y) {}
 
-Vector2 Vector2::operator+(const Vector2 &v) const {
-    return Vector2(x + v.x, y + v.y);
+vector2 vector2::operator+(const vector2 &v) const {
+    return vector2(x + v.x, y + v.y);
 }
 
-Vector2 &Vector2::operator+=(const Vector2 &v) {
+vector2 &vector2::operator+=(const vector2 &v) {
     x += v.x;
     y += v.y;
     return *this;
 }
 
-Vector2 Vector2::operator-(const Vector2 &v) const {
-    return Vector2(x - v.x, y - v.y);
+vector2 vector2::operator-(const vector2 &v) const {
+    return vector2(x - v.x, y - v.y);
 }
 
-Vector2 &Vector2::operator-=(const Vector2 &v) {
+vector2 &vector2::operator-=(const vector2 &v) {
     x -= v.x;
     y -= v.y;
     return *this;
 }
 
-float &Vector2::operator[](int i) {
+float &vector2::operator[](int i) {
     assert(i == 1 || i == 0);
     switch (i) {
         case 0:
--- math/vector2.hpp
+++ math/vector2.hpp
@@ -2,17 +2,17 @@
 #define TEXTO_VECTOR2_HPP
 
 
-class Vector2 {
+class vector2 {
     public:
         float x,y;
 
-        explicit Vector2(float x=0, float y=0);
-        Vector2(const Vector2& vector);
+        explicit vector2(float x=0, float y=0);
+        vector2(const vector2 & vector);
 
-        Vector2 operator+ (const Vector2 &v) const;
-        Vector2 & operator+= (const Vector2 &v);
-        Vector2 operator- (const Vector2 &v) const;
-        Vector2& operator-= (const Vector2 &v);
+        vector2 operator+ (const vector2 &v) const;
+        vector2 & operator+= (const vector2 &v);
+        vector2 operator- (const vector2 &v) const;
+        vector2 & operator-= (const vector2 &v);
 
         float &operator[](int i);
 };

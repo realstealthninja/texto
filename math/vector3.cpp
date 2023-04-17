ab5c3f40ce4d9bdc9c17d2f498b1e9ce74f92287
realstealthninja <realstealthninja@gmail.com>
2023-04-17 21:29:29 +0530

    fix: lower the vector

--- math/vector3.cpp
+++ math/vector3.cpp
@@ -1,32 +1,39 @@
 #include "vector3.hpp"
-#include "Vector4.hpp"
+#include "vector4.hpp"
 #include <cassert>
+#include <math.h>
 
-Vector3::Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
-Vector3::Vector3(const Vector3 &vector) {
+vector3::vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+vector3::vector3(const vector3 &vector) {
     x = vector.x;
     y = vector.y;
     z = vector.z;
 }
-Vector3 Vector3::operator+(const Vector3 &v) const {
-    return Vector3(x + v.x, y + v.y, z + v.z);
+vector3 vector3::operator+(const vector3 &v) const {
+    return vector3(x + v.x, y + v.y, z + v.z);
 }
-Vector3 &Vector3::operator+=(const Vector3 &v) {
+vector3 &vector3::operator+=(const vector3 &v) {
     x += v.x;
     y += v.y;
     z += v.z;
     return *this;
 }
-Vector3 Vector3::operator-(const Vector3 &v) const {
-    return Vector3(x-v.x, y-v.y, z-v.z);
+vector3 vector3::operator-(const vector3 &v) const {
+    return vector3(x-v.x, y-v.y, z-v.z);
 }
-Vector3 &Vector3::operator-=(const Vector3 &v) {
+vector3 &vector3::operator-=(const vector3 &v) {
     x -= v.x;
     y -= v.y;
     z -= v.z;
     return *this;
 }
-float &Vector3::operator[](int i) {
+vector3 &vector3::operator*=(const vector3 &v) {
+    x *= v.x;
+    y *= v.y;
+    z *= v.z;
+    return *this;
+}
+float &vector3::operator[](int i) {
     assert(i == 2 || i == 1 || i == 0);
     switch(i) {
         case 0:
@@ -38,19 +45,28 @@ float &Vector3::operator[](int i) {
 
     }
 }
-Vector3 Vector3::TransformCoordinate(Vector3 &coordinate, Matrix<float> &transform) {
-    Vector4 vector = Vector4();
+vector3 vector3::TransformCoordinate(vector3 &coordinate, Matrix<float> &transform) {
+    vector4 vector = vector4();
     vector.x = (coordinate.x * transform(1,1)) + (coordinate.y * transform(2,1)) + (coordinate.z * transform(3,1)) + transform(4,1);
     vector.y = (coordinate.x * transform(1,2)) + (coordinate.y * transform(2,2)) + (coordinate.z * transform(3,2)) + transform(4,2);
     vector.z = (coordinate.x * transform(1,3)) + (coordinate.y * transform(2,3)) + (coordinate.z * transform(3,3)) + transform(4,3);
     vector.w = 1.0f / (coordinate.x * transform(1,4)) + (coordinate.y * transform(2,4)) + (coordinate.z * transform(3,4)) + transform(4,4);
 
-    return Vector3(vector.x * vector.w, vector.y * vector.w, vector.z * vector.w);
+    return vector3(vector.x * vector.w, vector.y * vector.w, vector.z * vector.w);
 }
-Vector3 Vector3::cross(Vector3 &v) {
-    return Vector3(
+vector3 vector3::cross(vector3 &v) {
+    return vector3(
             (y * v.z) - (z *v.y),
             (z * v.x) - (x * v.z),
             (x * v.y) - (y * v.x)
     );
+}
+void vector3::normalize() {
+    float length = std::sqrt((x*x) + (y*y) + (z*z));
+    if (length != 0){
+        float inverse = 1.0f / length;
+        x *= inverse;
+        y *= inverse;
+        z *= inverse;
+    }
 }
--- math/vector3.hpp
+++ math/vector3.hpp
@@ -4,22 +4,24 @@
 
 #include "matrix.hpp"
 
-class Vector3 {
+class vector3 {
 public:
     float x, y, z;
-    explicit Vector3(float x = 0, float y = 0, float z = 0);
-    Vector3(const Vector3 &vector);
+    explicit vector3(float x = 0, float y = 0, float z = 0);
+    vector3(const vector3 &vector);
 
-    Vector3 operator+(const Vector3 &v) const;
-    Vector3 &operator+=(const Vector3 &v);
-    Vector3 operator-(const Vector3 &v) const;
-    Vector3 &operator-=(const Vector3 &v);
+    vector3 operator+(const vector3 &v) const;
+    vector3 &operator+=(const vector3 &v);
+    vector3 operator-(const vector3 &v) const;
+    vector3 &operator-=(const vector3 &v);
+    vector3 &operator*= (const vector3 &v);
 
     float &operator[](int i);
 
     // other operations
-    Vector3 TransformCoordinate(Vector3 &coordinate, Matrix<float> &transform);
-    Vector3 cross(Vector3 &v);
+    vector3 TransformCoordinate(vector3 &coordinate, Matrix<float> &transform);
+    vector3 cross(vector3 &v);
+    void normalize();
 };
 
 #endif//TEXTO_VECTOR3_HPP

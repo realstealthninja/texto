653d7a321c7a0e60d0f773cc1f7caf5198cef1dd
realstealthninja <realstealthninja@gmail.com>
2023-04-18 20:44:26 +0530

    fix: add more functionality to vector4

--- math/vector4.cpp
+++ math/vector4.cpp
@@ -1,1 +1,12 @@
 #include "vector4.hpp"
+#include <complex>
+
+vector4 vector4::toRotationAxis(vector3 axis, float angle) {
+    axis.normalize();
+    axis *= std::sin(angle * 0.5f);
+   return {
+        axis,
+        std::cos(angle * 0.5f)
+   };
+}
+

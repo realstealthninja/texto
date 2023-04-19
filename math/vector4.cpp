42c808b96696bac56fb54e342c4a0a6ea495af63
realstealthninja <realstealthninja@gmail.com>
2023-04-19 20:09:42 +0530

    fix: defining rotationYawPitchRoll

--- math/vector4.cpp
+++ math/vector4.cpp
@@ -9,4 +9,22 @@ vector4 vector4::toRotationAxis(vector3 axis, float angle) {
         std::cos(angle * 0.5f)
    };
 }
+vector4 vector4::rotationYawPitchRoll(float yaw, float pitch, float roll) {
+    float halfRoll = roll * 0.5f;
+    float halfPitch = pitch *0.5f;
+    float halfYaw = yaw * 0.5f;
 
+    float sinRoll = std::sin(halfRoll);
+    float cosRoll = std::cos(halfRoll);
+    float sinPitch = std::sin(halfPitch);
+    float cosPitch = std::cos(halfPitch);
+    float sinYaw = std::sin(halfYaw);
+    float cosYaw = std::sin(halfYaw);
+
+
+    return {
+            (cosYaw * sinPitch * cosRoll) + (sinYaw * cosPitch * sinRoll),
+            (sinYaw * cosPitch * cosRoll) - (cosYaw * sinPitch * sinRoll),
+            (cosYaw * cosPitch * sinRoll) - (sinYaw * sinPitch * cosRoll),
+            (cosYaw * cosPitch * cosRoll) + (sinYaw * sinPitch * sinRoll)};
+}

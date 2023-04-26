7472e543f3ea89001416f0f88a7d0c2ac9f52adc
realstealthninja <realstealthninja@gmail.com>
2023-04-26 17:09:10 +0530

    fix: use cmath instead of math.h

--- math/vector3.cpp
+++ math/vector3.cpp
@@ -1,7 +1,7 @@
 #include "vector3.hpp"
 #include "vector4.hpp"
 #include <cassert>
-#include <math.h>
+#include <cmath>
 
 vector3::vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 vector3::vector3(const vector3 &vector) {
@@ -42,17 +42,9 @@ float &vector3::operator[](int i) {
             return y;
         case 2:
             return z;
-
+        default:
+            return x;
     }
-}
-vector3 vector3::TransformCoordinate(vector3 &coordinate, Matrix<float> &transform) {
-    auto vector = vector4(1);
-    vector.x = (coordinate.x * transform(1,1)) + (coordinate.y * transform(2,1)) + (coordinate.z * transform(3,1)) + transform(4,1);
-    vector.y = (coordinate.x * transform(1,2)) + (coordinate.y * transform(2,2)) + (coordinate.z * transform(3,2)) + transform(4,2);
-    vector.z = (coordinate.x * transform(1,3)) + (coordinate.y * transform(2,3)) + (coordinate.z * transform(3,3)) + transform(4,3);
-    vector.w = 1.0f / (coordinate.x * transform(1,4)) + (coordinate.y * transform(2,4)) + (coordinate.z * transform(3,4)) + transform(4,4);
-
-    return vector3(vector.x * vector.w, vector.y * vector.w, vector.z * vector.w);
 }
 vector3 vector3::cross(vector3 &v) const {
     return vector3(

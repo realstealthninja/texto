6344a535a934c062a2a619487ca380a0c087e5a2
realstealthninja <realstealthninja@gmail.com>
2023-04-26 17:08:37 +0530

    feat: make scales actually constant and global

---
+++ scales.cpp
@@ -0,0 +1,6 @@
+#include "scales.hpp"
+namespace constants {
+    extern const std::string shortGrayscale  = " .:-=+*#%@";
+    extern const std::string grayscale = " .'`^\",:;Il!i><~+_-?][}{1)(|/\\tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+    extern const std::string reversedGrayscale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+}
--- scales.hpp
+++ scales.hpp
@@ -1,8 +1,11 @@
-#pragma once
+
 #ifndef TEXTO_SCALES_HPP
 #define TEXTO_SCALES_HPP
 
-std::string SHORT_GRAYSCALE = " .:-=+*#%@";
-std::string GRAYSCALE = " .'`^\",:;Il!i><~+_-?][}{1)(|/\\tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-std::string REVERSED_GRAYSCALE = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+#include <string>
+namespace constants {
+extern const std::string shortGrayscale;
+extern const std::string grayscale;
+extern const std::string reversedGrayscale;
+}
 #endif //TEXTO_SCALES_HPP

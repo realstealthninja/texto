033f938b491f9117e25bfb5a9ae13e182596047f
realstealthninja <realstealthninja@gmail.com>
2023-04-26 17:11:00 +0530

    fix: make 3d actually 3d

--- 3d.hpp
+++ 3d.hpp
@@ -3,41 +3,49 @@
 #define TEXTO_3D_HPP
 
 
-struct vector3 {
-    float x;
-    float y;
-    float z;
-};
-
-struct vector2 {
-    float x;
-    float y;
-};
-
+#include "math/Matrix.hpp"
+#include "math/vector2.hpp"
+#include "math/vector3.hpp"
+#include "rgba.hpp"
+#include <string>
 
 namespace textoEngine {
-    class Camera {
+    class camera {
         public:
-            vector3 Position{};
-            vector3 Target{};
-            Camera(vector3 position, vector3 target);
+            vector3 position{};
+            vector3 target{};
+            camera(vector3 position, vector3 target);
             [[nodiscard]] vector3 getPosition() const;
             [[nodiscard]] vector3 getTarget() const;
     };
 
 
-    class Mesh {
+    class mesh {
         public:
-            std::string Name;
-            vector3 Position{};
-            vector3 Rotation{};
-            std::vector<vector3> Vertices;
-            Mesh(
+            std::string name;
+            vector3 position{};
+            vector3 rotation{};
+            std::vector<vector3> vertices;
+            mesh(
                     std::string name,
                     vector3 position,
                     vector3 rotation,
                     std::vector<vector3> vertices
             );
+            explicit mesh(std::string name);
+    };
+    class renderer {
+    public:
+        std::vector<std::vector<std::string> > buffer;
+        int width;
+        int height;
+        renderer(int width, int height);
+        void clear(rgba color);
+        void present();
+        void putText(int x, int y , rgba colour);
+        [[nodiscard]] vector2 project(const vector3& coordinates, const Matrix& transformationMatrix) const;
+        void drawText(vector2 &point);
+        void render(camera &cam, std::vector<mesh> &meshes);
     };
 }
 

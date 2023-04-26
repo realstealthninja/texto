b40dbeabd8796c6ee156850e9a66d0e032edae02
realstealthninja <realstealthninja@gmail.com>
2023-04-26 17:09:40 +0530

    fix: rename rgba

---
+++ rgba.hpp
@@ -0,0 +1,14 @@
+
+#ifndef TEXTO_RGBA_HPP
+#define TEXTO_RGBA_HPP
+
+
+class rgba {
+public:
+    int r,g,b,a;
+    rgba(int r, int g, int b, int a) : r(r) , g(g), b(b), a(a) {};
+
+};
+
+
+#endif//TEXTO_RGBA_HPP

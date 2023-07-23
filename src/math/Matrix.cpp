81c3c48d72f9b2c6351661e34180a83a8c51813b
realstealthninja <realstealthninja@gmail.com>
2023-07-23 18:36:19 +0530

    refactor: entirety of matrix

--- src/math/Matrix.cpp
+++ src/math/Matrix.cpp
@@ -208,87 +208,118 @@ unsigned Matrix::getCols() const {
     return this->cols;
 }
 
-Matrix Matrix::lookAt(vector3 &eye, vector3 &target, vector3 &up) const {
-    vector3 xAxis, yAxis, zAxis;
-    zAxis = target - eye;
-    zAxis.normalize();
-    xAxis = up.cross(zAxis);
-    xAxis.normalize();
-    yAxis = zAxis.cross(xAxis);
+Matrix Matrix::identity() {
+    Matrix result(4, 4, 0.0f);
 
-    Matrix result = Matrix(this->getRows(), this->getCols(), 0.0f);
-    result(2,2) = 1.0f;
-    result(3,3) = 1.0f;
-    result(4,4) = 1.0f;
-    result(1,1) = xAxis.x; result(2,1) = xAxis.y; result(3,1) = xAxis.z;
-    result(1,2) = yAxis.x; result(2,2) = yAxis.y; result(3,2) = yAxis.z;
-    result(1,3) = zAxis.x; result(2,3) = zAxis.y; result(3,3) = zAxis.z;
+    result.matrix.at(0).at(0) = 1.0f;
+    result.matrix.at(1).at(1) = 1.0f;
+    result.matrix.at(2).at(2) = 1.0f;
+    result.matrix.at(3).at(3) = 1.0f;
     return result;
 }
 
-void Matrix::perspectiveFovRh(
+
+Matrix Matrix::lookAt(vector3 &eye, vector3 &target, vector3 &up) {
+    vector3 xAxis, yAxis, zAxis;
+    zAxis = target - eye;
+    xAxis = up.cross(zAxis);
+    yAxis = zAxis.cross(xAxis);
+    zAxis.normalize();
+    xAxis.normalize();
+    yAxis.normalize();
+
+    Matrix result = Matrix(xAxis, yAxis, zAxis);
+    result.matrix[0][3] = -xAxis.dot(eye);
+    result.matrix[1][3] = -yAxis.dot(eye);
+    result.matrix[2][3] = -zAxis.dot(eye);
+    return result;
+}
+
+void Matrix::perspectiveFovLh(
         float fov,
         float aspect,
         float zNear,
         float zFar)
 {
-        auto yScale = (float)(1.0f / std::tan(fov * 0.5f));
-        float q = zFar / (zNear - zFar);
+        auto yScale = static_cast<float>(1.0f / std::tan(fov * 0.5f));
+        float depth = zFar - zNear;
 
         this->matrix[0][0] = yScale / aspect;
         this->matrix[1][1] = yScale;
-        this->matrix[2][2] = q;
-        this->matrix[2][3] = -1.0f;
-        this->matrix[3][2] = q * zNear;
+        this->matrix[2][2] = -zFar / depth;
+        this->matrix[2][3] = 1.0f;
+        this->matrix[3][2] = (zFar * zNear) / depth;
+}
+Matrix Matrix::rotation_pitch(float pitch) {
+    Matrix roo = Matrix::identity();
+    const float cos = std::cos(pitch);
+    const float sin = std::sin(pitch);
+    roo.matrix[0][0] = 1.0f;
+    roo.matrix[1][1] = cos;
+    roo.matrix[1][2] = -sin;
+    roo.matrix[2][1] = sin;
+    roo.matrix[2][2] = cos;
+    roo.matrix[3][3] = 1.0f;
+    return roo;
 }
 
-
-Matrix Matrix::rotationQuaternion(vector4 quaternion) {
-    Matrix result(4,4, 0.0);
-    
-    float xx = quaternion.x * quaternion.x;
-    float yy = quaternion.y * quaternion.y;
-    float zz = quaternion.z * quaternion.z;
-    float xy = quaternion.x * quaternion.y;
-    float zw = quaternion.z * quaternion.w;
-    float zx = quaternion.z * quaternion.x;
-    float yw = quaternion.y * quaternion.w;
-    float yz = quaternion.y * quaternion.z;
-    float xw = quaternion.x * quaternion.w;
-
-    result(1,1) = 1.0f - (2.0f * yy);
-    result(1,2) = 2.0f * (xy + zw);
-    result(1,3) = 2.0f * (zx - yw);
-    result(2,1) = 2.0f * (xy - zw);
-    result(2,2) = 1.0f - (2.0f * (zz + xx));
-    result(2,3) = 2.0f * (yz + xw);
-    result(3,1) = 2.0f * (zx + yw);
-    result(3,2) = 2.0f * (yz - xw);
-    result(3,3) = 1.0f - (2.0f * (yy +xx));
-    result(4,4) = 1.0;
-    return result;
+Matrix Matrix::rotation_roll(float roll) {
+    Matrix roo = Matrix::identity();
+    const float cos = std::cos(roll);
+    const float sin = std::sin(roll);
+    roo.matrix[0][0] = cos;
+    roo.matrix[0][1] = sin;
+    roo.matrix[1][0] = -sin;
+    roo.matrix[1][1] = cos;
+    roo.matrix[2][2] = 1.0f;
+    roo.matrix[3][3] = 1.0f;
+    return roo;
 }
 
+Matrix Matrix::rotation_yaw(float yaw) {
+    Matrix roo = Matrix::identity();
+    const float cos = std::cos(yaw);
+    const float sin = std::sin(yaw);
+    roo.matrix[0][0] = cos;
+    roo.matrix[0][2] = -sin;
+    roo.matrix[1][1] = 1.0f;
+    roo.matrix[2][0] = sin;
+    roo.matrix[2][2] = cos;
+    roo.matrix[3][3] = 1.0f;
+    return roo;
+}
 
 Matrix Matrix::rotationYawPitchRoll(float yaw, float pitch, float roll) {
-    vector4 quaternion(0.0);
-    quaternion = quaternion.rotationYawPitchRoll(yaw, pitch, roll);
-    Matrix result(4,4, 0.0f);
-    result = rotationQuaternion(quaternion);
+    Matrix rotated_matrix = Matrix::identity();
+    rotated_matrix *= Matrix::rotation_roll(roll);
+    rotated_matrix *= Matrix::rotation_pitch(pitch);
+    rotated_matrix *= Matrix::rotation_yaw(yaw);
+    return rotated_matrix;
+}
+
+Matrix Matrix::rotationYawPitchRoll(const vector3& v) {
+    return rotationYawPitchRoll(v.y, v.x, v.z);
+}
+
+Matrix Matrix::translation(const vector3& v) {
+    Matrix result = Matrix::identity();
+
+    result.matrix[3][0] = v.x;
+    result.matrix[3][1] = v.y;
+    result.matrix[3][2] = v.z;
     return result;
 }
 
-Matrix Matrix::translation(vector3 v) {
-    Matrix result(4,4, 0.0);
-    result(1,1) = 1.0f;
-    result(2,2) = 1.0f;
-    result(3,3) = 1.0f;
-    result(4,4) = 1.0f;
-
-    result(4,1) = v.x;
-    result(4,2) = v.y;
-    result(4,3) = v.z;
+Matrix Matrix::scale(const float x, const float y, const float z) {
+    Matrix result = Matrix(4, 4, 0.0f);
+    result.matrix[0][0] = x;
+    result.matrix[1][1] = y;
+    result.matrix[2][2] = z;
+    result.matrix[3][3] = 1.0f;
     return result;
+}
+Matrix Matrix::scale(const vector3& v) {
+    return Matrix::scale(v.x, v.y, v.z);
 }
 vector3 Matrix::transformCoordinate(const vector3& v) const {
     auto vector = vector4(1,1,1,1);
@@ -297,5 +328,15 @@ vector3 Matrix::transformCoordinate(const vector3& v) const {
     vector.z = (v.x * this->matrix[0][2]) + (v.y * this->matrix[1][2]) + (v.z * this->matrix[2][2]) + this->matrix[3][2];
     vector.w = 1.0f / (v.x * this->matrix[0][3]) + (v.y * this->matrix[1][3]) + (v.z * this->matrix[2][3]) + this->matrix[3][3];
 
-    return vector3(vector.x * vector.w, vector.y * vector.w, vector.z * vector.w);
+    return vector3(vector.x * vector.w, vector.y * vector.w, vector.z *  vector.w);
+}
+
+Matrix::Matrix(const vector3& top,const vector3& middle,const vector3& bottom) {
+    Matrix result(4,4, 0.0f);
+    this->rows = result.getRows();
+    this->cols = result.getCols();
+    this->matrix = result.matrix;
+    this->matrix[0] = *new std::vector<float>{top.x, top.y, top.z};
+    this->matrix[1] = *new std::vector<float>{middle.x, middle.y, middle.z};
+    this->matrix[2] = *new std::vector<float>{bottom.x, bottom.y, bottom.z};
 }

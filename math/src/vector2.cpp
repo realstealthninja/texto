f94f237b8e8663d94547187f892a36a24796bae8
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-06-27 22:02:21 +0530

    feat: add math source

---
+++ math/src/vector2.cpp
@@ -0,0 +1,37 @@
+#include "vector2.hpp"
+#include <cassert>
+
+vector2::vector2(float x, float y) : x(x), y(y) {};
+vector2::vector2(const vector2 &vector) : x(vector.x), y(vector.y) {};
+
+vector2 vector2::operator+(const vector2 &v) const {
+    return vector2(x + v.x, y + v.y);
+}
+
+vector2 &vector2::operator+=(const vector2 &v) {
+    x += v.x;
+    y += v.y;
+    return *this;
+}
+
+vector2 vector2::operator-(const vector2 &v) const {
+    return vector2(x - v.x, y - v.y);
+}
+
+vector2 &vector2::operator-=(const vector2 &v) {
+    x -= v.x;
+    y -= v.y;
+    return *this;
+}
+
+float &vector2::operator[](int i) {
+    assert(i == 1 || i == 0);
+    switch (i) {
+        case 0:
+            return x;
+        case 1:
+            return y;
+        default:
+            return x;
+    }
+}
---
+++ math/src/vector3.cpp
@@ -0,0 +1,80 @@
+#include "vector3.hpp"
+#include <cassert>
+#include <cmath>
+
+vector3::vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+vector3::vector3(const vector3 &vector) {
+    x = vector.x;
+    y = vector.y;
+    z = vector.z;
+}
+vector3 vector3::operator+(const vector3 &v) const {
+    return vector3(x + v.x, y + v.y, z + v.z);
+}
+
+vector3 &vector3::operator=(const vector3 &v) {
+    x = v.x;
+    y = v.y;
+    z = v.z;
+    return *this;
+}
+
+vector3 &vector3::operator+=(const vector3 &v) {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    return *this;
+}
+vector3 vector3::operator-(const vector3 &v) const {
+    return vector3(x-v.x, y-v.y, z-v.z);
+}
+vector3 &vector3::operator-=(const vector3 &v) {
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+    return *this;
+}
+vector3 &vector3::operator*=(const vector3 &v) {
+    x *= v.x;
+    y *= v.y;
+    z *= v.z;
+    return *this;
+}
+float &vector3::operator[](int i) {
+    assert(i == 2 || i == 1 || i == 0);
+    switch(i) {
+        case 0:
+            return x;
+        case 1:
+            return y;
+        case 2:
+            return z;
+        default:
+            return x;
+    }
+}
+vector3 vector3::cross(vector3 &v) const {
+    return vector3(
+            (y * v.z) - (z *v.y),
+            (z * v.x) - (x * v.z),
+            (x * v.y) - (y * v.x)
+    );
+}
+vector3 &vector3::operator*=(const float &n) {
+    x *= n;
+    y *= n;
+    z *= n;
+    return *this;
+}
+void vector3::normalize() {
+    float length = std::sqrt((x*x) + (y*y) + (z*z));
+    x /= length;
+    y /= length;
+    z /= length;
+}
+vector3 vector3::operator-() const {
+    return vector3(-x, -y, -z);
+}
+float vector3::dot(vector3 &v) const {
+    return (x*v.x + y*v.y + z*v.z);
+}

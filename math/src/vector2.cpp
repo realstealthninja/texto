17fee4c6f85e8de73913e51e4667814fd1344ef2
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-08-17 20:56:35 +0530

    feat: declare vector insertion

--- math/src/vector2.cpp
+++ math/src/vector2.cpp
@@ -1,8 +1,13 @@
 #include "vector2.h"
 #include <cassert>
+#include <format>
+#include <ostream>
 
 vector2::vector2(float x, float y) : x(x), y(y) {};
 vector2::vector2(const vector2 &vector) : x(vector.x), y(vector.y) {};
+
+vector2::vector2(const std::vector<float>& vec) : x(vec[0]), y(vec[1]) {};
+
 
 vector2 vector2::operator+(const vector2 &v) const {
     return vector2(x + v.x, y + v.y);
@@ -22,6 +27,11 @@ vector2 &vector2::operator-=(const vector2 &v) {
     x -= v.x;
     y -= v.y;
     return *this;
+}
+
+std::ostream& operator<<(std::ostream &out, const vector2& vec) {
+    out << std::format("({}, {})", vec.x, vec.y);
+    return out;
 }
 
 float &vector2::operator[](int i) {

0f3e6db4ad3daccda1b185c55f2741d9ce65bad1
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-08-17 20:56:57 +0530

    feat: Use home grown math

--- src/3d.cpp
+++ src/3d.cpp
@@ -1,3 +1,5 @@
+#include <cstddef>
+#include <cstdlib>
 #include <string>
 #include <iostream>
 #include <cmath>
@@ -21,9 +23,18 @@ namespace textoEngine {
         this->width = width;
         this->height = height;
         this->buffer = std::vector<std::vector<std::string> > {};
+        buffer.resize(height);
+        for (int i{0}; i < height; i++) {
+            buffer[i].resize(width);
+            for(int j{0}; j < width; j++) {
+                buffer[i][j] = " ";
+            }
+        }
     }
 
     void renderer::clear(color colour) {
+        static_cast<void>(system("clear"));
+        
         for (int i{0}; i < height; i++) {
             std::vector<std::string> line;
             for (int j{0}; j < width; j++) {
@@ -35,25 +46,25 @@ namespace textoEngine {
                         constants::grayscale[colour.a % constants::grayscale.length()])
                 );
             }
-            buffer.push_back(line);
+            buffer[i] = line;
         }
     }
 
     void renderer::present() {
-        static_cast<void>(system("clear"));
-        for (int i{0}; i < height; i++) {
-            for (int j{0}; j < width; j++){
+        for (std::size_t i{0}; i < buffer.size(); i++) {
+            for (std::size_t j{0}; j < buffer[i].size(); j++){
                 std::cout << buffer.at(i).at(j);
             }
             std::cout << "\n";
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds((int)1000/60));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     void renderer::putText(int x, int y, color colour) {
-        buffer[x][y].assign(cv::format(
-                "\033[48;2;%i;%i;%im%c\033[0m",
-                colour.r,
+        this->buffer.at(x).at(y).assign( 
+            std::format(
+                "\033[48;2;{};{};{}m{}\033[0m",
+                colour.r, 
                 colour.g,
                 colour.b,
                 constants::grayscale[colour.a % constants::grayscale.length()]));
@@ -61,33 +72,53 @@ namespace textoEngine {
 
 
     void renderer::drawText(vector2 &point) {
-        color yellow = {105, 225, 215, 45};
+        color yellow = {255, 0, 0, 2};
         if (point.x >= 0 && point.y >= 0 && point.x < (float) width && point.y < (float) height) {
             putText(std::floor(point.x), std::floor(point.y), yellow);
+            
+        } else {
+            std::cout << std::format("out of range x: {} y:{} width:{} height:{}\n", point.x, point.y, width, height);
         }
     }
 
 
     void renderer::render(std::vector<vector3> &meshes) {
-        auto projectionMatrix = Matrix(3, 2, 0.0f);
+        auto projectionMatrix = Matrix(2, 3, 0.0f);
 
         // convert the null projection matrix into a non square identity matrix
         // to ignore the z coordinate
         //
         //  0   0   0  becomes  1  0  0
         //  0   0   0           0  1  0 
+
+
+        //  multiply given coordinate matrix by projection matrix
+        //   1   0   0      x   0   0       x   0   0
+        //   0   1   0  *   0   y   0   =   0   y   0
+        //                  0   0   z  
+        
     
         projectionMatrix(1,1) = 1.0f;
         projectionMatrix(2,2) = 1.0f;
 
 
-
-
         for (const auto& mesh : meshes) {
-            Matrix mesh_mat(mesh);
-            auto result = projectionMatrix * mesh_mat;
-            vector2 result_vec = vector2(result(1,1), result(2,1));
-            drawText(result_vec);
+            auto object_matrix = Matrix(3, 3, 0.0f);
+
+            object_matrix(1, 1) = mesh.x;
+            object_matrix(2, 2) = mesh.y;
+            object_matrix(3, 3) = mesh.z;
+
+            auto resultant_matrix = projectionMatrix * object_matrix;
+
+            // convert result matrix to vector 2;
+
+            auto resultant_vector = vector2(object_matrix.diagonalVector());
+            resultant_vector.x =  (((resultant_vector.x + 1)* 0.5) * (width-1));
+            resultant_vector.y += std::abs((1-((resultant_vector.y +1) * 0.5)) * (height-1));
+            //std::clog << resultant_vector << std::endl;
+
+            drawText(resultant_vector);
         }
     }
 

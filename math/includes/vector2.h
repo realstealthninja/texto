9e0ae507aba915154a8b29a797947fc90dfe3920
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-08-17 20:54:47 +0530

    feat: add inseration overload

--- math/includes/vector2.h
+++ math/includes/vector2.h
@@ -2,12 +2,16 @@
 #define TEXTO_VECTOR2_H
 
 
+#include <ostream>
+#include <vector>
+
 class vector2 {
     public:
         float x, y;
 
         explicit vector2(float x=0, float y=0);
-        vector2(const vector2 & vector);
+        vector2(const vector2& vector);
+        vector2(const std::vector<float>& vec);
 
         vector2 operator+ (const vector2 &v) const;
         vector2 & operator+= (const vector2 &v);
@@ -15,6 +19,8 @@ class vector2 {
         vector2 & operator-= (const vector2 &v);
 
         float &operator[](int i);
+
+        friend std::ostream& operator<< (std::ostream& out, const vector2 &vec);
 };
 
 #endif //TEXTO_VECTOR2_H

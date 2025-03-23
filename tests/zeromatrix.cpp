0b327950a5dec2b22a855b176b5f8c46c565af1b
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-03-23 11:41:38 +0530

    fix: add more tests

--- tests/zeromatrix.cpp
+++ tests/zeromatrix.cpp
@@ -3,11 +3,38 @@
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("Zero matrix is zero matrix", "[matrix]") {
-    auto matrix1 = mat<3, 3>::ZERO();
+    auto zero_matrix = mat<3, 3>::ZERO();
 
     for(size_t i = 0; i < 3; i++) {
         for(size_t j = 0; j < 3; j++) {
-            REQUIRE(matrix1[i, j] == 0.0f);
+            REQUIRE(zero_matrix[i, j] == 0.0f);
         }
     } 
+}
+
+TEST_CASE("Transpose of a zero matrix is still a zero matrix", "[matrix]") {
+    auto zero_matrix = mat<3, 3>::ZERO();
+    zero_matrix = zero_matrix.transpose();
+
+    for(size_t i = 0; i < 3; i++) {
+        for(size_t j = 0; j < 3; j++) {
+            REQUIRE(zero_matrix[i, j] == 0.0f);
+        }
+    } 
+}
+
+TEST_CASE(
+    "Transpose of a non square zero",
+    "[matrix]"
+) {
+    mat<3, 2> zero_mat = mat<3, 2>::ZERO();
+    mat<2, 3> zero_mat_t = zero_mat.transpose();
+
+
+    for (size_t i = 0; i < 2; i++) {
+        for (size_t j = 0; j < 3; j++) {
+            REQUIRE(zero_mat_t[i, j] == 0.0f);
+        }
+    }
+
 }

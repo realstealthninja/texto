c1e75d724e56bb05643704626ddfd9ccf384bdbe
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-03-25 00:07:34 +0530

    doc(libtexto): add docs to image and utils

--- libtexto/includes/texto/image.h
+++ libtexto/includes/texto/image.h
@@ -8,15 +8,37 @@
 
 typedef std::vector<std::vector<std::string> >  array2d_string;
 
+/**
+ * @brief a wrapper class of cv::Mat
+ * 
+ */
 class image {
+private:
+    cv::Mat img; ///< opencv image
 public:
+    /**
+     * @brief Construct a new image object from cv::mat
+     * 
+     * @param img the opencv image to construct an object from
+     */
     image(cv::Mat img) : img(img) {};
 
+    /**
+     * @brief resizes the image to the given width and height
+     * 
+     * @param width new width
+     * @param height new height
+     */
     void resize(int width, int height);
+
+    /**
+     * @brief converts the image to ascii
+     * 
+     * @return std::shared_ptr<array2d_string> 
+     */
     std::shared_ptr<array2d_string> to_ascii();
     
-private:
-    cv::Mat img;
+
 };
 
 #endif // TEXTO_IMAGE_H
--- libtexto/includes/texto/utils.h
+++ libtexto/includes/texto/utils.h
@@ -9,6 +9,12 @@
 #include <opencv2/opencv.hpp>
 #include <texto/image.h>
 
+/**
+ * @brief reads an image into image object
+ * 
+ * @param path to the image
+ * @return image 
+ */
 inline image read_image(const std::string& path) {
     cv::Mat img = cv::imread(path, cv::IMREAD_UNCHANGED);
     cv::cvtColor(img, img, cv::COLOR_BGR2BGRA);

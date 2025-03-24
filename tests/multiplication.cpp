f240a4752def6575dc43c5fe0dad0b8abf3c5b00
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-03-24 23:31:47 +0530

    tests: add test for multiplication

---
+++ tests/multiplication.cpp
@@ -0,0 +1,34 @@
+#include <catch2/catch_test_macros.hpp>
+#include <texto-math/mat.h>
+
+TEST_CASE("Multiplication with zero matrix", "[matrix]") {
+    auto mat1 = mat<3, 3>::ZERO();
+    std::array<std::array<float, 3>, 3> mat_arr(
+        {
+            std::array<float, 3>({3, 9, 8}),
+            std::array<float, 3>({2, 7, 6}),
+            std::array<float, 3>({1, 5, 4})
+        }
+    );
+    mat<3, 3> mat2(mat_arr);
+
+    auto res = mat1 * mat2;
+
+    REQUIRE(res == mat1);
+}
+
+TEST_CASE("Multiplication with Identity matrix", "[matrix]") {
+    auto mat1 = mat<3, 3>::IDENTITY();
+    std::array<std::array<float, 3>, 3> mat_arr(
+        {
+            std::array<float, 3>({3, 9, 8}),
+            std::array<float, 3>({2, 7, 6}),
+            std::array<float, 3>({1, 5, 4})
+        }
+    );
+    mat<3, 3> mat2(mat_arr);
+
+    auto res = mat1 * mat2;
+
+    REQUIRE(res == mat2);
+}

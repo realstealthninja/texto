35c0a8c69db71ab49e4dea0bb80e98b053ee61d4
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-03-26 17:58:33 +0530

    test: add test for accessing the matrix

---
+++ tests/access.cpp
@@ -0,0 +1,22 @@
+#include <catch2/catch_test_macros.hpp>
+#include <stdexcept>
+#include <texmath/mat.h>
+
+texmath::mat<3, 3> matrix = {
+    {1.0f , 2.0f , 3.0f},
+    {-2.0f, -5.0f, 3.0f},
+    {10.0f, 1.0f, 0.5f}
+};
+
+TEST_CASE("Invalid access into matrix via () operator", "[matrix]") {
+    REQUIRE_THROWS_AS(matrix(0, 0), std::invalid_argument);
+    REQUIRE_THROWS_AS(matrix(4, 4), std::invalid_argument);
+    REQUIRE_THROWS_AS(matrix(4, 3), std::invalid_argument);
+    REQUIRE_THROWS_AS(matrix(3, 4), std::invalid_argument);
+}
+
+TEST_CASE("Invalid access into matrix via [] operator", "[matrix]") {
+    REQUIRE_THROWS_AS(matrix(4, 4), std::invalid_argument);
+    REQUIRE_THROWS_AS(matrix(4, 3), std::invalid_argument);
+    REQUIRE_THROWS_AS(matrix(3, 4), std::invalid_argument);
+}

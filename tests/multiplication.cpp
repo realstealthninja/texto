fa8dcc5cbc897f6bf323a187fcb859a25906607d
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-03-27 13:27:41 +0530

    test: add test for multiplication with scalar

--- tests/multiplication.cpp
+++ tests/multiplication.cpp
@@ -33,3 +33,23 @@ TEST_CASE("Multiplication with Identity matrix", "[matrix]") {
 
     REQUIRE(res == mat2);
 }
+
+TEST_CASE("Multiplication with a scalar quantity", "[matrix]") {
+    std::array<std::array<float, 3>, 3> mat_arr(
+        {
+            std::array<float, 3>({3, 9, 8}),
+            std::array<float, 3>({2, 7, 6}),
+            std::array<float, 3>({1, 5, 4})
+        }
+    );
+    mat<3, 3> mat2(mat_arr);
+
+    auto res = mat2 * 2;
+
+    for(size_t i = 0 ; i < 3; i++) {
+        for(size_t j = 0; j < 3; j++) {
+            REQUIRE(res[i, j] == (mat2[i,j] * 2));
+        }
+    }
+}
+
